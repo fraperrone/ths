b9d7d35b339d07550d82c61ed8a4a12d54667247
Fran <fperronerey@gmail.com>
2026-03-21 17:28:54 -0300

    actualizacion navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Navbar, Nav, Container, Button } from 'react-bootstrap'
 
 import ModalAsesoria from './ModalAsesoria'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom' // importamos useNavigate para navegación programática
 
 export default function MyNavbar() {
   // Estado que controla si el modal está abierto o cerrado
@@ -10,6 +11,15 @@ export default function MyNavbar() {
   // Funciones que cambian el estado
   const handleOpen = () => setShowModal(true) // abre el modal
   const handleClose = () => setShowModal(false) // cierra el modal
+
+  // uilizamos useNavigate
+  const navigate = useNavigate()
+
+  // hacemos navigate generico
+  const handleNavigate = (path) => {
+    navigate(path) // navega a la ruta especificada
+  }
+
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
@@ -20,11 +30,18 @@ export default function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Inicio</Nav.Link>
-            <Nav.Link href="#servicios">Servicios</Nav.Link>
+            <Nav.Link onClick={() => handleNavigate('/')}>
+              Inicio
+            </Nav.Link>
+            {/* Servicio en mantenimiento */}
+            {/* <Nav.Link onClick={() => handleNavigate('/servicios')}>
+              Servicios
+            </Nav.Link> */}
             {/* Para futuro: */}
             {/* <Nav.Link href="#proyectos">Proyectos</Nav.Link> */}
-            <Nav.Link href="#contacto">Contacto</Nav.Link>
+            <Nav.Link onClick={() => handleNavigate('/contacto')}>
+              Contacto
+            </Nav.Link>
             <Button variant="outline-light" className="ms-3" onClick={handleOpen}>
               Solicitar Asesoría
             </Button>
